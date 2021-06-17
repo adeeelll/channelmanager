9fcdf3243468590fdc8ec4df3eeca1462c4536fa
hassan <hassanmumtaz0@gmail.com>
2021-06-17 23:05:22 +0500

    Changes

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
         'mysql' => array(
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'forge',
-            'username' => 'forge',
-            'password' => '',
+            'database' => 'channelmanager',
+            'username' => 'newuser',
+            'password' => 'Pakistan_1',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
